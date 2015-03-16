d2952aa3e9e0d8d077b2f10e77f44d07ea5b8e7c
Ender <that.joshua.green@gmail.com>
2015-03-16 22:11:38 +0000

    Derped between stuff in this, need to sleep :/

--- EB3IRC.py
+++ EB3IRC.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 #! -*- coding: utf-8 -*-
 
+# Standard library imports
 import ssl
 
 # To get the library this bot uses run pip3 install irc
 import irc.bot, irc.connection
 
 class EB3( irc.bot.SingleServerIRCBot ):
-    """docstring for EB3"""
+    """Usage: EB3( config=dict, data=dict, wrSelf=self ) """
     def __init__( self, 
-            config={ server="localhost", port=6667, useSSL=False, sPass="", nickname="EB3", realname="EB3", channel=""},
-            data,
-            wrSelf ):
-        if config.useSSL:
+            wrSelf,
+            config,
+            data ):
+        if config.useSSL: #Whether to use SSL or not
             ssl_factory = irc.connection.Factory( wrapper=ssl.wrap_socket )
             irc.bot.SingleServerIRCBot.__init__( self, 
                     [(config.server, config.port)], 
-                    config.nickname,
-                    config.realname, 
+                    config.nickname, config.realname, 
                     connect_factory=ssl_factory)
         else:
             irc.bot.SingleServerIRCBot.__init__( self,
                 [(config.server, config.port)],
-                config.nickname,
-                config.realname)
+                config.nickname, config.realname)
         self.data = data
-        self.wrapper = wrSelf
+        self.config = config
+        self.wrapper = wrSelf # Allows bot module to call / contact wrapper
--- wrapper.py
+++ wrapper.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 #! -*- coding: utf-8 -*-
 
+import EB3IRC
+
+bots = dict()
+
+GlobalData = dict()
+
+if __name__ == "__main__":
+	bID = 1
+	for IRCBot in config.IRC:
+		# { srv="localhost", prt=6667, useSSL=False, sPass="", nName="EB3", rName="EB3"}
+		bots[ bID ] = EB3IRC.EB3( GlobalData, IRCBot )
+		bID = bID + 1
